973af1e1b325c35dcbebe766680322f0299ee450
MrD <1051408857@qq.com>
2024-08-07 15:56:29 +0800

    错误修改

--- dzy/wallet/src/main/java/com/dzyproject/wallet/service/TransactionService.java
+++
@@ -1,9 +0,0 @@
-package com.dzyproject.wallet.service;
-
-import com.dzyproject.wallet.entity.Transaction;
-
-import java.util.List;
-
-public interface TransactionService {
-    List<Transaction> getTransactionsByAccountId(Integer accountId);
-}
